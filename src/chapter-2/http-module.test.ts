49317b98ed8c61bbf284315be88f0f11a2891c19
Wojciech Karas <wkaras89@gmail.com>
2017-11-05 08:48:18 +0100

    Add tests using typemoq

--- src/chapter-2/http-module.test.ts
+++ src/chapter-2/http-module.test.ts
@@ -1,7 +1,23 @@
+import * as http from "http";
 import { handleRequest } from "./http-module";
+import * as TypeMoq from "typemoq";
 
 describe("handle request", () => {
-  it("fake", () => {
-    expect(true).toBe(true);
+  it("Get method", () => {
+    const responseMock = TypeMoq.Mock.ofType<http.ServerResponse>();
+    const requestMock = TypeMoq.Mock.ofType<http.IncomingMessage>();
+
+    requestMock.setup(x => x.method).returns(() => "GET");
+
+    handleRequest(requestMock.object, responseMock.object);
+
+    responseMock.verify(
+      x =>
+        x.writeHead(200, {
+          "Content-Type": "text/plain",
+        }),
+      TypeMoq.Times.once(),
+    );
+    responseMock.verify(x => x.end("GET Request\n"), TypeMoq.Times.once());
   });
 });
