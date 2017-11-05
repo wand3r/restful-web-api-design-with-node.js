4550eb8ab7f59fad2b5e59819be98d05b2978287
Wojciech Karas <wkaras89@gmail.com>
2017-11-05 06:56:23 +0100

    add http server with http methods

---
+++ src/hello-node-http-server.ts
@@ -0,0 +1,52 @@
+import * as http from "http";
+import { RequestHandler } from "./http-types";
+
+const port = 8180;
+
+const plainTextResponse = (text: string) => (response: http.ServerResponse) => {
+  response.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+  response.end(text);
+};
+
+const handleGetRequest = plainTextResponse("GET Request\n");
+const handlePOSTRequest = plainTextResponse("POST Request\n");
+const handlePUTRequest = plainTextResponse("PUT Request\n");
+const handleDELETERequest = plainTextResponse("DELETE Request\n");
+const handleHEADRequest = plainTextResponse("HEAD Request\n");
+
+const handleBadRequest = (res: http.ServerResponse) => {
+  res.writeHead(400, {
+    "Content-Type": "text/plain",
+  });
+  res.end("Bad request\n");
+};
+
+const handleRequest: RequestHandler = (req, res) => {
+  switch (req.method) {
+    case "GET":
+      handleGetRequest(res);
+      break;
+    case "POST":
+      handlePOSTRequest(res);
+      break;
+    case "PUT":
+      handlePUTRequest(res);
+      break;
+    case "DELETE":
+      handleDELETERequest(res);
+      break;
+    case "HEAD":
+      handleHEADRequest(res);
+      break;
+    default:
+      handleBadRequest(res);
+      break;
+  }
+  console.log("Request processing ended");
+};
+
+http.createServer(handleRequest).listen(port, "localhost");
+
+console.log("Started Node.js http server at http://localhost:" + port);
